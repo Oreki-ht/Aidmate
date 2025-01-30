60b414cbb8b4d90ebafc6618ac7fa8391aa58fb4
oreki <condou23@gmail.com>
2025-01-30 09:51:54 +0300

    added treatment singleton

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IParamedicService, ParamedicService>();
 builder.Services.AddSingleton<IAmbulanceService, AmbulanceService>();
 builder.Services.AddSingleton<IPatientService, PatientService>();
+builder.Services.AddSingleton<ITreatmentService, TreatmentService>();
 var app = builder.Build();
 
 // Enable Swagger UI in Development Mode
